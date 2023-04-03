e229883282b4f47b440e0b78a40d6a45ee3ad1f5
Mateus Santana <mateus.santana@liferay.com>
2023-04-03 15:49:41 -0300

    LPS-180610 Type correctly

--- modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ObjectRelationship/ObjectRelationshipFormBase.tsx
+++ modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ObjectRelationship/ObjectRelationshipFormBase.tsx
@@ -116,7 +116,7 @@ export function ObjectRelationshipFormBase({
 	>();
 
 	const [objectDefinitions, setObjectDefinitions] = useState<
-		ObjectDefinition[]
+		Partial<ObjectDefinition>[]
 	>([]);
 	const [query, setQuery] = useState<string>('');
 
@@ -224,7 +224,7 @@ export function ObjectRelationshipFormBase({
 				value={selectedType}
 			/>
 
-			<AutoComplete<ObjectDefinition>
+			<AutoComplete<Partial<ObjectDefinition>>
 				creationLanguageId={
 					creationLanguageId as Liferay.Language.Locale
 				}
@@ -284,11 +284,3 @@ interface IPros {
 	setValues: (values: Partial<ObjectRelationship>) => void;
 	values: Partial<ObjectRelationship>;
 }
-
-type ObjectDefinition = {
-	externalReferenceCode: string;
-	id: number;
-	label: LocalizedValue<string>;
-	name: string;
-	system: boolean;
-};
