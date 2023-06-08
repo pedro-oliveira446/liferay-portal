1cb34a7c34ec4f7cc7390304faecb9e6f0167798
Pedro Leite <pedro.leite@liferay.com>
2023-06-08 11:08:07 -0300

    LPS-184962 Add parameters to rich text field acept localized values

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/java/com/liferay/dynamic/data/mapping/form/field/type/internal/rich/text/RichTextDDMFormFieldTemplateContextContributor.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/java/com/liferay/dynamic/data/mapping/form/field/type/internal/rich/text/RichTextDDMFormFieldTemplateContextContributor.java
@@ -17,15 +17,28 @@ package com.liferay.dynamic.data.mapping.form.field.type.internal.rich.text;
 import com.liferay.dynamic.data.mapping.form.field.type.DDMFormFieldTemplateContextContributor;
 import com.liferay.dynamic.data.mapping.form.field.type.constants.DDMFormFieldTypeConstants;
 import com.liferay.dynamic.data.mapping.form.field.type.internal.util.DDMFormFieldTypeUtil;
+import com.liferay.dynamic.data.mapping.model.DDMForm;
 import com.liferay.dynamic.data.mapping.model.DDMFormField;
+import com.liferay.dynamic.data.mapping.model.LocalizedValue;
 import com.liferay.dynamic.data.mapping.render.DDMFormFieldRenderingContext;
 import com.liferay.portal.kernel.editor.configuration.EditorConfiguration;
 import com.liferay.portal.kernel.editor.configuration.EditorConfigurationFactory;
+import com.liferay.portal.kernel.json.JSONException;
+import com.liferay.portal.kernel.json.JSONFactory;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.portlet.RequestBackedPortletURLFactoryUtil;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
 
+import java.util.Locale;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -49,19 +62,56 @@ public class RichTextDDMFormFieldTemplateContextContributor
 		DDMFormField ddmFormField,
 		DDMFormFieldRenderingContext ddmFormFieldRenderingContext) {
 
+		DDMForm ddmForm = ddmFormField.getDDMForm();
+
+		JSONObject localeJSONObject = _getLocaleJSONObject(
+			ddmForm.getDefaultLocale());
+
+		boolean objectFieldLocalized =
+			Validator.isNotNull(
+				ddmFormField.getProperty("objectFieldLocalized")) &&
+			(boolean)ddmFormField.getProperty("objectFieldLocalized");
+
 		return HashMapBuilder.<String, Object>put(
+			"availableLocales",
+			JSONUtil.toJSONArray(
+				_language.getAvailableLocales(), this::_getLocaleJSONObject,
+				_log)
+		).put(
+			"defaultLocale", localeJSONObject
+		).put(
+			"editingLocale", localeJSONObject
+		).put(
+			"objectFieldLocalized", objectFieldLocalized
+		).put(
 			"predefinedValue",
-			DDMFormFieldTypeUtil.getPropertyValue(
-				ddmFormField, ddmFormFieldRenderingContext.getLocale(),
-				"predefinedValue")
+			() -> {
+				if (objectFieldLocalized) {
+					return _getPredefinedValue(
+						ddmFormField, ddmFormFieldRenderingContext);
+				}
+
+				return DDMFormFieldTypeUtil.getPropertyValue(
+					ddmFormField, ddmFormFieldRenderingContext.getLocale(),
+					"predefinedValue");
+			}
 		).put(
 			"value",
-			DDMFormFieldTypeUtil.getPropertyValue(
-				ddmFormFieldRenderingContext, "value")
+			() -> {
+				if (objectFieldLocalized) {
+					return _getValueJSONObject(ddmFormFieldRenderingContext);
+				}
+
+				return DDMFormFieldTypeUtil.getPropertyValue(
+					ddmFormFieldRenderingContext, "value");
+			}
 		).putAll(
 			_getData(ddmFormFieldRenderingContext, ddmFormField.getType())
 		).build();
 	}
+
+	@Reference
+	protected JSONFactory jsonFactory;
 
 	private Map<String, Object> _getData(
 		DDMFormFieldRenderingContext ddmFormFieldRenderingContext,
@@ -90,7 +140,60 @@ public class RichTextDDMFormFieldTemplateContextContributor
 		return editorConfiguration.getData();
 	}
 
+	private JSONObject _getLocaleJSONObject(Locale locale) {
+		JSONObject jsonObject = jsonFactory.createJSONObject();
+
+		String languageId = LocaleUtil.toLanguageId(locale);
+
+		jsonObject.put(
+			"displayName", locale.getDisplayName(locale)
+		).put(
+			"icon",
+			StringUtil.toLowerCase(StringUtil.replace(languageId, '_', "-"))
+		).put(
+			"localeId", languageId
+		);
+
+		return jsonObject;
+	}
+
+	private String _getPredefinedValue(
+		DDMFormField ddmFormField,
+		DDMFormFieldRenderingContext ddmFormFieldRenderingContext) {
+
+		LocalizedValue localizedValue = ddmFormField.getPredefinedValue();
+
+		if (localizedValue == null) {
+			return null;
+		}
+
+		return localizedValue.getString(
+			ddmFormFieldRenderingContext.getLocale());
+	}
+
+	private JSONObject _getValueJSONObject(
+		DDMFormFieldRenderingContext ddmFormFieldRenderingContext) {
+
+		try {
+			return jsonFactory.createJSONObject(
+				ddmFormFieldRenderingContext.getValue());
+		}
+		catch (JSONException jsonException) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(jsonException);
+			}
+		}
+
+		return jsonFactory.createJSONObject();
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		RichTextDDMFormFieldTemplateContextContributor.class);
+
 	@Reference
 	private EditorConfigurationFactory _editorConfigurationFactory;
+
+	@Reference
+	private Language _language;
 
 }
