bc68c825ddcd558df4366c30e2ec2f5be362d66d
Pedro Leite <pedro.leite@liferay.com>
2023-08-11 16:55:40 -0300

    LPS-193374 Add new configurantion

---
+++ modules/apps/object/object-api/src/main/java/com/liferay/object/configuration/ObjectScriptConfiguration.java
@@ -0,0 +1,31 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.object.configuration;
+
+import aQute.bnd.annotation.metatype.Meta;
+
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+
+/**
+ * @author Pedro Leite
+ */
+@ExtendedObjectClassDefinition(
+	category = "object", scope = ExtendedObjectClassDefinition.Scope.SYSTEM
+)
+@Meta.OCD(
+	id = "com.liferay.object.configuration.ObjectScriptConfiguration",
+	localization = "content/Language", name = "object-script-configuration-name"
+)
+public interface ObjectScriptConfiguration {
+
+	@Meta.AD(
+		deflt = "false",
+		description = "allow-administrators-execute-script-help",
+		name = "allow-administrators-execute-script", required = false
+	)
+	public boolean allowInstanceAdminExecuteCode();
+
+}
