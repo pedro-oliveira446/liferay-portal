b0b6385d549b98b6dcf7d287a6056e3bbcf561c2
Pedro Leite <pedro.leite@liferay.com>
2023-09-03 22:40:38 -0300

    LPS-193364 Show source in the picklist table

--- modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/list/type/frontend/data/set/view/table/ListTypeDefinitionsTableFDSView.java
+++ modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/list/type/frontend/data/set/view/table/ListTypeDefinitionsTableFDSView.java
@@ -35,6 +35,8 @@ public class ListTypeDefinitionsTableFDSView extends BaseTableFDSView {
 			"name", "name",
 			fdsTableSchemaField -> fdsTableSchemaField.setContentRenderer(
 				"actionLink")
+		).add(
+			"system", "source"
 		).build();
 	}
 
