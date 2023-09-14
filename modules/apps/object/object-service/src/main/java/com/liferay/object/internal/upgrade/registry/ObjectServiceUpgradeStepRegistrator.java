65bca28a5d44f6a2a2eff81d853fec3f0569e22a
Pedro Leite <pedro.leite@liferay.com>
2023-09-14 13:31:24 -0300

    LPS-196070 Upgrade Process

--- modules/apps/object/object-service/src/main/java/com/liferay/object/internal/upgrade/registry/ObjectServiceUpgradeStepRegistrator.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/internal/upgrade/registry/ObjectServiceUpgradeStepRegistrator.java
@@ -340,6 +340,19 @@ public class ObjectServiceUpgradeStepRegistrator
 			"8.1.0", "8.2.0",
 			new com.liferay.object.internal.upgrade.v8_2_0.
 				ObjectValidationRuleUpgradeProcess());
+
+		registry.register(
+			"8.2.0", "8.3.0",
+			new BaseExternalReferenceCodeUpgradeProcess() {
+
+				@Override
+				protected String[][] getTableAndPrimaryKeyColumnNames() {
+					return new String[][] {
+						{"ObjectValidationRule", "objectValidationRuleId"}
+					};
+				}
+
+			});
 	}
 
 	@Reference
