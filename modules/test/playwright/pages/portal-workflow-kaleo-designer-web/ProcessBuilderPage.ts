52a6e756e58e9f1d4d4b540f44b54a888f46078b
Pedro Leite <pedro.leite@liferay.com>
2024-03-24 15:17:00 -0300

    LPD-16281 Create ProcessBuilder page

---
+++ modules/test/playwright/pages/portal-workflow-kaleo-designer-web/ProcessBuilderPage.ts
@@ -0,0 +1,31 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import {Page} from '@playwright/test';
+
+import {ApplicationsMenuPage} from '../product-navigation-applications-menu/ApplicationsMenuPage';
+
+export class ProcessBuilderPage {
+	readonly applicationsMenuPage: ApplicationsMenuPage;
+	readonly page: Page;
+
+	constructor(page: Page) {
+		this.applicationsMenuPage = new ApplicationsMenuPage(page);
+		this.page = page;
+	}
+
+	async clickWorkflowDefinitionName(name: string) {
+        await this.page
+			.getByRole('link', {
+				exact: true,
+				name,
+			})
+			.click();
+	}
+
+	async goto() {
+		await this.applicationsMenuPage.goToProcessBuilder();
+	}
+}
