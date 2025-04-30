1fa39da7cc00105e60c417d566df43317f170131
Pedro Leite <pedro.leite@liferay.com>
2025-04-30 14:00:16 -0300

    LPD-54352 Add update Status method

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryVersionLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryVersionLocalServiceImpl.java
@@ -15,6 +15,9 @@ import com.liferay.portal.aop.AopService;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.json.JSONFactory;
 import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.search.Indexable;
+import com.liferay.portal.kernel.search.IndexableType;
+import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.vulcan.dto.converter.DTOConverterRegistry;
 
@@ -44,12 +47,38 @@ public class ObjectEntryVersionLocalServiceImpl
 			objectEntry.getVersion() + 1);
 	}
 
+	@Indexable(type = IndexableType.REINDEX)
+	@Override
+	public ObjectEntryVersion updateStatus(
+		long userId, ObjectEntryVersion objectEntryVersion, int status,
+		ServiceContext serviceContext)
+		throws PortalException {
+
+		if (objectEntryVersion.getStatus() == status) {
+			return objectEntryVersion;
+		}
+
+		ObjectEntry originalObjectEntry = (ObjectEntry)objectEntryVersion.clone();
+
+		objectEntryVersion.setStatus(status);
+
+//		User user = _userLocalService.getUser(userId);
+//		objectEntryVersion.setStatusByUserId(user.getUserId());
+//		objectEntryVersion.setStatusByUserName(user.getFullName());
+//		objectEntryVersion.setStatusDate(serviceContext.getModifiedDate(null));
+
+
+		// TODO if IS THE ONLY VERSION SET THE ENTRY TO EXPIRED !
+
+		return objectEntryVersionPersistence.update(objectEntryVersion);
+	}
+
 	@Override
 	public ObjectEntryVersion deleteObjectEntryVersion(
 			long objectEntryId, int version)
 		throws PortalException {
 
-		if (getObjectEntryVersionsCount(objectEntryId) <= 1) {
+		if (getObjectEntryVersionsCount(objectEntryId) == 1) {
 
 			// TODO validation
 
