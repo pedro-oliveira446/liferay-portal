b7a5cfd9bd1a0ead5f557f1b0ed17675a51280b0
Pedro Leite <pedro.leite@liferay.com>
2025-11-26 10:08:12 -0300

    LPD-72586 Integration test

---
+++ modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/internal/model/listener/test/ObjectEntryModelListenerTest.java
@@ -0,0 +1,32 @@
+package com.liferay.object.internal.model.listener.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+public class ObjectEntryModelListenerTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new LiferayIntegrationTestRule();
+
+	@Test
+	public void testOnAfterCreate() throws Exception {
+
+	}
+	@Test
+	public void testOnAfterRemove() throws Exception {
+
+	}
+
+	@Test
+	public void testOnAfterUpdate() throws Exception {
+
+	}
+}
